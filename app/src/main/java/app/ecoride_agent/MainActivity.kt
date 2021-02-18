32ba97b1ac40d9823cdff3765cfa8e7a5c77e246
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-02-18 07:13:19 +0200

    Set up home

--- app/src/main/java/app/ecoride_agent/MainActivity.kt
+++ app/src/main/java/app/ecoride_agent/MainActivity.kt
@@ -2,10 +2,56 @@ package app.ecoride_agent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.findFragment
+import app.ecoride_agent.databinding.ActivityMainBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        mainBinding.drawerController.setOnClickListener {
+            openCloseNavigationDrawer()
+        }
+        mainBinding.navView.bringToFront()
+
+        val map = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        map.apply {
+            getMapAsync(this@MainActivity)
+            onCreate(null)
+            onResume()
+        }
+
+    }
+
+    private fun openCloseNavigationDrawer() {
+        if (mainBinding.mainDrawer.isDrawerOpen(GravityCompat.START)){
+            mainBinding.mainDrawer.closeDrawer(GravityCompat.START)
+        }else{
+            mainBinding.mainDrawer.openDrawer(GravityCompat.START)
+        }
+    }
+
+    private fun closeNavigationDrawer(){
+        mainBinding.mainDrawer.closeDrawer(GravityCompat.START)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+
     }
 }
