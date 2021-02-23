4ee6a41726452265741928b15712905d44fb342f
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-02-23 23:45:13 +0200

    Settings Minor UI

--- app/src/main/java/app/ecoride_agent/ui/settings/SettingsActivity.kt
+++ app/src/main/java/app/ecoride_agent/ui/settings/SettingsActivity.kt
@@ -2,11 +2,26 @@ package app.ecoride_agent.ui.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import app.ecoride_agent.R
+import app.ecoride_agent.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var settingsBinding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        settingsBinding = DataBindingUtil.setContentView(this, R.layout.activity_settings)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        settingsBinding.settingsBack.setOnClickListener {
+            this@SettingsActivity.onBackPressed()
+        }
     }
 }
