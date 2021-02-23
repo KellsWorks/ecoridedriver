7b80a3faa148cf976a4dc883c898114e1f7c6fad
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-02-23 23:27:53 +0200

    Earnings Activity🎪

--- app/src/main/java/app/ecoride_agent/ui/earnings/EarningsActivity.java
+++ app/src/main/java/app/ecoride_agent/ui/earnings/EarningsActivity.java
@@ -1,16 +1,34 @@
 package app.ecoride_agent.ui.earnings;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import app.ecoride_agent.R;
+import app.ecoride_agent.databinding.ActivityEarningsBinding;
 
 public class EarningsActivity extends AppCompatActivity {
+
+    private ActivityEarningsBinding earningsBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_earnings);
+
+        earningsBinding = DataBindingUtil.setContentView(this, R.layout.activity_earnings);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        earningsBinding.earningsBack.setOnClickListener(this::navigateUp);
+    }
+
+    public void navigateUp(View v)
+    {
+        this.onBackPressed();
     }
 }
