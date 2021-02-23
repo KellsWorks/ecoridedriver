f0b0f576084bab117f28576e9810c7ba7e3cdc63
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-02-23 23:34:59 +0200

    Summary UI

--- app/src/main/java/app/ecoride_agent/ui/summary/SummaryActivity.kt
+++ app/src/main/java/app/ecoride_agent/ui/summary/SummaryActivity.kt
@@ -2,11 +2,27 @@ package app.ecoride_agent.ui.summary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import app.ecoride_agent.R
+import app.ecoride_agent.databinding.ActivitySummaryBinding
 
 class SummaryActivity : AppCompatActivity() {
+
+    private lateinit var summaryBinding: ActivitySummaryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_summary)
+
+        summaryBinding = DataBindingUtil.setContentView(this, R.layout.activity_summary)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        summaryBinding.summaryBack.setOnClickListener {
+            this@SummaryActivity.onBackPressed()
+        }
+
     }
 }
