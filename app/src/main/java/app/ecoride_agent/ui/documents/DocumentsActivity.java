0c28f58d73f5335d903b9aa2b5f01a174d51a520
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-02-23 23:41:20 +0200

    Documents UI

--- app/src/main/java/app/ecoride_agent/ui/documents/DocumentsActivity.java
+++ app/src/main/java/app/ecoride_agent/ui/documents/DocumentsActivity.java
@@ -1,16 +1,34 @@
 package app.ecoride_agent.ui.documents;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import app.ecoride_agent.R;
+import app.ecoride_agent.databinding.ActivityDocumentsBinding;
 
 public class DocumentsActivity extends AppCompatActivity {
+
+    private ActivityDocumentsBinding documentsBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_documents);
+
+        documentsBinding = DataBindingUtil.setContentView(this, R.layout.activity_documents);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        documentsBinding.dpcumentsBack.setOnClickListener(this::navigateUp);
+    }
+
+    public void navigateUp(View view){
+        this.onBackPressed();
     }
 }
