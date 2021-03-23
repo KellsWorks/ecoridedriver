9082a70e9b8f9995e206bb34bf2de2bfc7f7b84c
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-03-24 00:45:44 +0200

    REFACTORING PROFILE

--- app/src/main/java/app/ecoride_agent/ui/profile/ProfileActivity.kt
+++ app/src/main/java/app/ecoride_agent/ui/profile/ProfileActivity.kt
@@ -2,11 +2,29 @@ package app.ecoride_agent.ui.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import app.ecoride_agent.R
+import app.ecoride_agent.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var profileBinding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+
+        profileBinding = DataBindingUtil.setContentView(this, R.layout.activity_profile)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        profileBinding.profileBack.setOnClickListener {
+            onBackPress()
+            }
+    }
+
+    private fun onBackPress(){
+        this.onBackPressed()
     }
 }
