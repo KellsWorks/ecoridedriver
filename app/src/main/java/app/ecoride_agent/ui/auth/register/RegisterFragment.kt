6c5cbccaccb19e40e77b3da8b3224924ca49a9cf
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-03-24 01:14:57 +0200

    Driver Registration

--- app/src/main/java/app/ecoride_agent/ui/auth/register/RegisterFragment.kt
+++ app/src/main/java/app/ecoride_agent/ui/auth/register/RegisterFragment.kt
@@ -6,10 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import app.ecoride_agent.R
+import app.ecoride_agent.customs.CustomProgressDialog
 import app.ecoride_agent.databinding.FragmentRegisterBinding
 import app.ecoride_agent.helpers.SharedHelper
+import app.ecoride_agent.network.ApiClient
+import app.ecoride_agent.network.ApiInterface
+import app.ecoride_agent.network.responses.register.Register
 import es.dmoral.toasty.Toasty
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import timber.log.Timber
 
 class RegisterFragment : Fragment() {
 
@@ -28,23 +35,108 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        registerBinding.toVerifyOTP.setOnClickListener {
-            if (validate()){
-                SharedHelper.putKey(requireContext(), "register_phone", registerBinding.registerPhoneGet.text.toString())
-                findNavController().navigate(R.id.action_registerFragment_to_registerDetailsFragment)
+
+        registerBinding.continueRegister.setOnClickListener{
+            if(validate()){
+                registerUser(
+                    registerBinding.registerUsername.text.toString(),
+                    registerBinding.registerPhone.text.toString(),
+                    registerBinding.registerEmail.text.toString(),
+                    registerBinding.registerPassword.text.toString(),
+                    registerBinding.registerConfirmPassword.text.toString(),
+                    registerBinding.emergencyPhone.text.toString()
+                )
             }
         }
-        registerBinding.registerClose.setOnClickListener {
+
+        registerBinding.registerBack.setOnClickListener {
             findNavController().navigateUp()
         }
     }
 
-    private fun validate() : Boolean{
-        return if (registerBinding.registerPhoneGet.text?.length != 9){
-            Toasty.warning(requireContext(), "Enter a 9 digit number").show()
+    private fun registerUser(
+        name: String,
+        phone: String,
+        email: String,
+        password : String,
+        c_password: String,
+        emergency: String
+    ) {
 
-            false
-        }else
-            true
+        val progressDialog = CustomProgressDialog()
+        progressDialog.show(requireContext(), "Registering...")
+
+        val api = ApiClient.client!!.create(ApiInterface::class.java)
+        val register: Call<Register> = api.register(name, phone, email, password, c_password, emergency, "ecoride_agent")
+
+        register.enqueue(object : Callback<Register?> {
+
+            override fun onFailure(call: Call<Register?>, t: Throwable) {
+                Timber.e(t)
+                Toasty.error(requireContext(), "A network error occurred").show()
+                progressDialog.dialog.dismiss()
+            }
+
+            override fun onResponse(call: Call<Register?>, response: Response<Register?>) {
+
+                progressDialog.dialog.dismiss()
+
+                when(response.code()){
+                    200 ->{
+                        SharedHelper.putKey(requireContext(), "remember_token", response.body()?.data?.token)
+                        Toasty.success(requireContext(), response.body()!!.message).show()
+                        findNavController().navigateUp()
+
+                    }else->{
+                    Timber.e(response.errorBody()!!.string())
+                }
+                }
+
+            }
+
+        })
     }
+
+    private fun validate(): Boolean{
+
+        if (registerBinding.registerUsername.text.toString().isEmpty()){
+
+            registerBinding.registerUsernameLayout.isErrorEnabled = true
+            registerBinding.registerUsernameLayout.error = "This field is required"
+
+            return false
+        }
+
+        if (registerBinding.registerPhone.text!!.length != 10){
+
+            Toasty.warning(requireContext(), "Phone number is required").show()
+
+            return false
+        }
+
+        if (registerBinding.emergencyPhone.text!!.length != 10){
+
+            registerBinding.emergencyPhoneLayout.isErrorEnabled = true
+            registerBinding.emergencyPhoneLayout.error = "Enter a 10 digit number"
+
+            return false
+        }
+
+        if (!registerBinding.registerEmail.text!!.contains("@")){
+
+            registerBinding.registerEmailLayout.isErrorEnabled = true
+            registerBinding.registerEmailLayout.error = "Enter a valid email addrerss"
+
+            return false
+        }
+        if (registerBinding.registerPassword.text.toString() != registerBinding.registerConfirmPassword.text.toString() ){
+
+            registerBinding.registerConfirmPasswordLayout.isErrorEnabled = true
+            registerBinding.registerConfirmPasswordLayout.error = "Passwords do not match"
+
+            return false
+        }
+        return true
+    }
+
 }
